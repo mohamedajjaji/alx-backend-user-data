adae5a92d7fa8e0b741689aae531539d5f601b8a
Mohamed AJJAJI <majjaji98@gmail.com>
2024-04-19 01:10:21 +0000

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,11 +2,26 @@
 """ Session authentication module for the API
 """
 from flask import request
-
+from uuid import uuid4
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """ Session authentication class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a session id for the user
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Retrieves the user id of the user associated with a given
+        session id
+        """
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
