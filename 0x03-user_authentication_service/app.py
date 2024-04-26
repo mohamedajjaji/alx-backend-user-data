4fedd945b72cf0a3149a0ab73de4673636c42e0e
Mohamed AJJAJI <majjaji98@gmail.com>
2024-04-26 00:42:53 +0000

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ A simple Flask app with user authentication features
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -45,6 +45,20 @@ def login() -> str:
     rsp = jsonify({"email": email, "message": "logged in"})
     rsp.set_cookie("session_id", session_id)
     return rsp
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """ DELETE /sessions
+    Return:
+        - Redirects to home route
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
